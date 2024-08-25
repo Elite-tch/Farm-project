8f2713c449aac1463846916fe7a5a1f32c9f1bd4
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-08-25 16:08:35 +0100

    Update Navbar.jsx

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,1 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Navbar = () => {
+    return (
+        <nav className="navbar shadow-lg fixed-top ">
+<div className="container-fluid"> 
+ <div className="nav-content"> 
+     
+ <div className="nav-brand">
+ <Link to="/" className="navbar-brand">Ceci<span>Farm</span></Link>  
+ </div>
+            
+   <div className="nav-link ">   
+<ul className="navbar-menu  ">  
+       <li>
+<Link to="/product"  className="navbar-link ">Products</Link>
+</li>
+       
+ <li>
+<Link to="/mission"  className="navbar-link ">Mission</Link>
+</li>
+       
+<li>
+ <Link to="/contact" className="navbar-link ">Contact</Link>
+</li>
+         </ul>
+        </div>
+     </div>
+             </div>
+        </nav>
+    );
+}
+
+export default Navbar;
