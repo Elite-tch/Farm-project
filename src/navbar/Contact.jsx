bbf7836d1f2695434a4ee2716ea2cebd1190f3bf
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-08-25 16:24:43 +0100

    Create Contact.jsx

---
+++ src/navbar/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ContactUs from './contact/contactpage.jsx';
+import Footer from './landingpage/footer.jsx';
+
+
+const Contact = () => {
+    return (
+        <div>
+            <ContactUs />
+        <Footer />
+        </div>
+    );
+}
+
+export default Contact;
