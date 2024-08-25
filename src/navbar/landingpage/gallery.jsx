98a052450b1e46c6fcda4c65dcf24e9831fe214e
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-08-25 16:30:59 +0100

    Create gallery.jsx

---
+++ src/navbar/landingpage/gallery.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import GalleryComponent from './gallerycomponent';
+
+function Gallery(){
+  return (
+    
+ 
+   <GalleryComponent
+     img1 =" coco.jpg"
+     img2 =" cocohead.jpg"
+     img3 ="img.jpg "
+     img4 =" img1.jpg"
+     img5 ="img2.jpg "
+     img6 =" cucu.jpg"
+     img7 =" pamtree1.jpg"
+     img8 =" img5.jpg"
+     img9 ="img3.jpg "
+     img10 ="img4.jpg "
+     img11 ="img5.jpg "
+     img12 =" pamtree2.jpg"
+     img13 =" shara.jpg"
+     img14 ="tomato.jpg"
+     img15 =" shop.jpg"
+     img16 =" img6.jpg"
+     img17 ="img7.jpg "
+     img18 ="img8.jpg "
+     img19 =" coconutr.jpg"
+     img20 =" img9.jpg"
+     img21 =" water.jpg"
+
+
+
+
+     />
+
+
+    
+
+    )}
+
+
+
+
+
+
+export default Gallery;
