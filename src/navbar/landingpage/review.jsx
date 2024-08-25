3668e78c1f8d751b350bb840cccd55e79c7018f7
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-08-25 16:45:46 +0100

    Create review.jsx

---
+++ src/navbar/landingpage/review.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReviewText from './reviewtext.jsx';
+import Footer from './footer.jsx';
+
+function Review() {
+
+  return(
+<div>
+
+
+    
+    <ReviewText 
+      title= "What Our Customers Say"
+      header1="Amazing Quality and Freshness! "
+      para1="I ordered some coconuts and ripe plantains from here, and they were incredibly fresh! The quality was top-notch, and the delivery was fast. I’ll definitely be ordering again soon! "
+      name1= "— Jane D. "
+      
+      header2="Highly Recommend This Farm "
+      para2="The watermelon and tomatoes I received were perfect. You can tell they were grown with care. It’s nice to support a local farm that delivers such high-quality produce. "
+      name2= "— Michael S. "
+      
+      header3="Great Experience from Start to Finish "
+      para3="I purchased some coconut heads and unripe plantains. The process was smooth, and the products were excellent. I appreciate the attention to detail and the friendly customer service. "
+      name3= " — Emily T."
+      
+      
+      />
+<Footer />
+
+
+
+</div>
+
+  
+    
+)}
+
+
+export default Review;
