45be757b83364d9502734ef0cd4f0cfa835f2050
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-08-27 11:53:56 +0100

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,31 +3,30 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar shadow-lg fixed-top ">
-<div className="container-fluid"> 
- <div className="nav-content"> 
-     
- <div className="nav-brand">
- <Link to="/" className="navbar-brand">Ceci<span>Farm</span></Link>  
- </div>
-            
-   <div className="nav-link ">   
-<ul className="navbar-menu  ">  
-       <li>
-<Link to="/product"  className="navbar-link ">Products</Link>
-</li>
-       
- <li>
-<Link to="/mission"  className="navbar-link ">Mission</Link>
-</li>
-       
-<li>
- <Link to="/contact" className="navbar-link ">Contact</Link>
-</li>
-         </ul>
-        </div>
-     </div>
-             </div>
+        <nav className="navbar shadow-lg fixed-top">
+            <div className="container-fluid">
+                <div className="nav-content">
+                    {/* Brand on the left */}
+                    <div className="nav-brand">
+                        <Link to="/" className="navbar-brand">Ceci<span>Farm</span></Link>
+                    </div>
+
+                    {/* Links on the right */}
+                    <div className="nav-link">
+                        <ul className="navbar-menu">
+                            <li>
+                                <Link to="/product" className="navbar-link">Products</Link>
+                            </li>
+                            <li>
+                                <Link to="/mission" className="navbar-link">Mission</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact" className="navbar-link">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 }
