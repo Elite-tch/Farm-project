79b1cb3545a93d0b4d5c71d4ea93163e4b467264
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-08-27 16:49:09 +0100

    Update missionpage.jsx

--- src/navbar/mission/missionpage.jsx
+++ src/navbar/mission/missionpage.jsx
@@ -19,19 +19,19 @@ const MissionPage = (props) => {
       
 <div className="mission-values ">
         <div className="row text-center">
-<div className="col-md-4 content shadow-lg ">
+<div className="col-md-4 content mx-2 shadow-lg ">
             <h3 className="values-title">{props.title1}</h3>
             <p className="values-description">
               {props.para1}
             </p>
           </div>
           
-          <div className="col-md-4 content shadow-lg">
+          <div className="col-md-4 mx-2 content shadow-lg">
             <h3 className="values-title">{props.title2}</h3>
    <p className="values-description"> {props.para2}</p>
           </div>
           
-          <div className="col-md-4 content shadow-lg">
+          <div className="col-md-4 mx-2 content shadow-lg">
  <h3 className="values-title">{props.title3}</h3>
 <p className="values-description"> {props.para3}</p>
           </div>
