3ce0aa45e11c5e0cf533cdee1e8dd1c24f20ab21
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2024-08-27 05:32:05 +0100

    Update greeting.jsx

--- src/navbar/landingpage/greeting.jsx
+++ src/navbar/landingpage/greeting.jsx
@@ -6,10 +6,10 @@ function Greeting(event) {
       return (
          <div>
            <div className="hero-section text-center pt-5">      
-   <h2 className="hero-tittle ">
-    {event.title} </h2>
-<h5 className="hero-subtittle">
- {event.subtitle}  </h5>
+   <h1 className="hero-tittle ">
+    {event.title} </h1>
+<h4 className="hero-subtittle">
+ {event.subtitle}  </h4>
   <p className="hero-text">     {event.texthero} </p>            
           <div className="hero-button">
 <button className="button">
